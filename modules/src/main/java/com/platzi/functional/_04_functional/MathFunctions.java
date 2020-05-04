192c6ac99fc5ea37117b08b542de684057889866
afcarrion <af.carrion@uniandes.edu.co>
2020-05-04 13:31:35 -0500

    Primer Commit con Function y Predicate

---
+++ modules/src/main/java/com/platzi/functional/_04_functional/MathFunctions.java
@@ -0,0 +1,44 @@
+package com.platzi.functional._04_functional;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class MathFunctions {
+	
+	public static void main(String[] main) {
+		Function<Integer, Integer> square = new Function<Integer, Integer>() {
+			@Override
+			public Integer apply(Integer x) {
+				return x*x;
+			}
+		};
+		System.out.println(square.apply(5));
+		
+		Function<Integer, Boolean> isOdd = x -> x % 2 == 1;
+		
+		Predicate<Integer> isEven = x -> x % 2 == 0;
+		
+		isEven.test(4);
+		
+		Predicate<Student> isApproved = student -> student.getCalificacion() >= 6.0;
+		
+		Student andres = new Student(5.9);
+		System.out.println(isApproved.test(andres));
+	}
+	
+	static class Student{
+		
+		private double calificacion;
+		
+		public Student(double calificacion) {
+			this.calificacion = calificacion;
+		}
+		
+		public double getCalificacion() {
+			return calificacion;
+		}
+	}
+	
+	
+	
+}
